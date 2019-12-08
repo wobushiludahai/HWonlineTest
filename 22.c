c530b19149a774ff85933465134c094007e5a4cd
luhai <2286875743@qq.com>
2019-12-08 22:58:34 +0800

    update a template

---
+++ 22.c
@@ -0,0 +1,15 @@
+/**
+ * @description: 汽水瓶
+ * @param {type} 
+ * @return: 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+
+    return 0;
+}
