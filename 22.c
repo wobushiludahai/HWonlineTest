968e75615cf5724c3eb88065872d60e22f6e5f36
landsea <wobushiludahai@163.com>
2019-12-09 14:14:54 +0800

    update 22.c

--- 22.c
+++ 22.c
@@ -8,10 +8,33 @@
 #include <string.h>
 #include <math.h>
 
-char testbuf[100] = {0};
+int testresult = 0;
+int testnum;
+int testcal(int test)
+{
+    int temp;
+    int returnnum = test/3;
+    temp = returnnum + test%3;
+    if((test == 3)||(test == 2))
+    {
+        return 1;
+    }
+    else if(test == 1)
+    {
+        return 0;
+    }
+    
+    return(returnnum + testcal(temp));
+}
 
 int main(void)
 {
-
+    while(scanf("%d", &testnum)!=EOF)
+    {
+        if(testnum!=0)
+        {
+            printf("%d\n", testcal(testnum));   
+        }
+    }
     return 0;
 }
