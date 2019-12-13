89a7470aea0af6801d015d44badbfe2420e043f3
landsea <wobushiludahai@163.com>
2019-12-14 00:16:00 +0800

    update 31.c

---
+++ 31.c
@@ -0,0 +1,53 @@
+
+/**
+ * @description: a template for test
+ * @param {type} 
+ * @return: 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char testbuf[10000] = {0};
+int testlen;
+int fuzhu;
+int i;
+
+int main(void)
+{    
+    while(gets(testbuf))
+    {
+        testlen = strlen(testbuf);
+        i = testlen - 1;
+        fuzhu = testlen;
+        for(;i>=0;i--)
+        {
+            if((testbuf[i] > 'z')||(testbuf[i] < 'A')||((testbuf[i] > 'Z')&&(testbuf[i] < 'a')))
+            {
+                for(int j = i + 1; j < fuzhu; j++)
+                {
+                    printf("%c", testbuf[j]);
+                }
+                if((((testbuf[i+1] >= 'a')&&(testbuf[i+1] <= 'z'))||((testbuf[i+1] >= 'A')&&(testbuf[i+1] <= 'Z')))&&(i!=0))
+                {
+                    printf(" ");
+                }
+
+                fuzhu = i;
+            }
+        }
+
+        if(((testbuf[0] >= 'a')&&(testbuf[0] <= 'z'))||((testbuf[0] >= 'A')&&(testbuf[0] <= 'Z')))
+        {
+            for(int j = 0; j < fuzhu; j++)
+            {
+                printf("%c", testbuf[j]);
+            }
+        }
+    }
+
+
+
+    return 0;
+}
