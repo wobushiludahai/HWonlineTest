96fd5ec84736723eced756341bc23867df4e61ec
luhai <2286875743@qq.com>
2019-12-18 23:23:50 +0800

    update and keep moving

---
+++ 43.c
@@ -0,0 +1,16 @@
+
+/**
+ * @description: a template for test
+ * @param {type} 
+ * @return: 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+
+    return 0;
+}
