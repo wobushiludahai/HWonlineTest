9e7c2c93696d78afa522f3cfe2374823ffd53025
landsea <wobushiludahai@163.com>
2019-12-26 14:50:56 +0800

    update 47.c

---
+++ 47.c
@@ -0,0 +1,36 @@
+
+/**
+ * @description: a template for test
+ * @param {type} 
+ * @return: 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int n,m;
+int N,A,M,B;
+
+int main(void)
+{
+    while(scanf("%d %d", &m, &n) != EOF)
+    {
+        scanf("%d %d", &M, &A);
+        printf("%d %d\n", M, A);
+        for(int i = 1; i < m; i++)
+        {
+            scanf("%d %d", &N, &B);
+            if(N == M) continue;
+            for(int j = 1; j < N - M; j++)
+            {
+                printf("%d %d\n",(M+j),(A+(B-A)/(N-M)*j));
+            }
+            printf("%d %d\n", N, B);
+            M = N;
+            A = B;
+        }
+    }
+
+    return 0;
+}
